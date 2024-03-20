1deb133770b36bc8bb0962dd3a6ddbfb523d578b
Nikhilkumar2311 <120698682+Nikhilkumar2311@users.noreply.github.com>
2024-03-20 09:24:33 +0530

    Shunting Yard Algorithm

--- No._Of_SubStrings.py
+++
@@ -1,7 +0,0 @@
-# Finding len of No. of distinct substring from a given string
-s = "abc"
-sub = {s[i:j] for i in range(len(s)) for j in range(i + 1, len(s) + 1)}
-print("No. of Substrings: ", len(sub))
-
-# for printing SubString itself
-print(sub)
