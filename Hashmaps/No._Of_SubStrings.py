818bbc881225c8df512235ce3f938a32d39d4a98
Nikhilkumar2311 <120698682+Nikhilkumar2311@users.noreply.github.com>
2024-03-31 20:41:02 +0530

    Subarrays with K Different Integers

--- Hashmaps/No._Of_SubStrings.py
+++ Hashmaps/No._Of_SubStrings.py
@@ -5,3 +5,12 @@ print("No. of Substrings: ", len(sub))
 
 # for printing SubString itself
 print(sub)
+
+# Or
+s = "abc"
+sub = set()
+for i in range(len(s)):
+    for j in range(i + 1, len(s) + 1):
+        sub.add(s[i:j])
+
+print("No. of Substrings: ", len(sub))
